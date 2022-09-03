5a9b3fbda60bd0d24c9857050dfffb0793c35cf6
delarosa0305 <delarosa2381i@gmail.com>
2022-09-03 11:37:19 -0400

    successfully tested componenets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-// import Gallery from './components/Gallery';
+import Gallery from './components/Gallery';
 
 function App() {
   const [categories] = useState([
@@ -25,7 +25,7 @@ function App() {
       ></Nav>
       <main>
         <div>
-          {/* <Gallery></Gallery> */}
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
       </main>
